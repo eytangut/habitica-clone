7228292f535fddf9de70df1da8d5ecfe0c9bc9bf
eytangut <eytangut@gmail.com>
2024-08-06 20:08:06 +0300

    feat: Remove unnecessary properties from Task class

--- lib/task.dart
+++ lib/task.dart
@@ -2,9 +2,7 @@
 class Task {
   String title;
   String description;
-  TaskType type;
-  IsCompleted completion = IsCompleted.no;
-  IsPositive positive;
+
   Task({
     required this.title,
     required this.description,
